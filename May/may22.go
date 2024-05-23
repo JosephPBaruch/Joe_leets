9f165cea585f954283fd648e186c78936cd7c871
Joseph Baruch <josephbaruch48@gmail.com>
2024-05-22 20:46:14 -0700

    may 22 coding...

---
+++ May/may22.go
@@ -0,0 +1,20 @@
+func addBinary(a string, b string) string {
+    var answer string
+    var length int
+    if len(a) > len(b) {
+        length = len(a) + 1
+    }else{
+        length = len(b) + 1
+    }
+    for i := 0; i < length; i++ { // loop through every character from right to left
+        if len(a)
+        
+            // if it doesnt exist -> 0
+            // add first char of both together
+            // if 0 then answer[i] = 0
+            // if 1 then answer[i] = 1
+            // if 2 then answer[i] = 0 and remainder is set 
+    }
+    
+    return answer
+}
